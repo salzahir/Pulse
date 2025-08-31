f09aa1c9daea5b8706270d790030f98a10f46601
Salman <salzahir.dev@gmail.com>
2025-08-30 21:39:46 -0500

    feat: connect WebSocket to database - messages now saved on receive

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,6 +1,7 @@
 // socket.ts
 import { WebSocketServer, WebSocket } from 'ws';
 import type { IncomingMessage } from 'http';
+import { saveMessage } from './db/messageDb';
 
 export const wss = new WebSocketServer({ noServer: true });
 
@@ -8,9 +9,22 @@ export const setupWebSocket = () => {
   wss.on('connection', (ws: WebSocket, req: IncomingMessage) => {
     console.log('New WebSocket client connected');
 
-    ws.on('message', (msg: WebSocket) => {
-      console.log('Message received:', msg.toString());
+    ws.on('message', async (msg: Buffer) => {
+      const timeStamp = Date.now();
+      console.log('Message received:', msg.toString(), timeStamp.toString());
+      const message = msg.toString();
+      const savedMessage = await saveMessage(
+        message,
+        'test-user-id',
+        'fake-conversation-id',
+        timeStamp
+      );
+      console.log('Message saved to database' + savedMessage);
       ws.send(`Server received: ${msg}`);
+    });
+
+    ws.on('error', (err: Error) => {
+      console.error('WebSocket error:', err);
     });
 
     ws.on('close', () => {
